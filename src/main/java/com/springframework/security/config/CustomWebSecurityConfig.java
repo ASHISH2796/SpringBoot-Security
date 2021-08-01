71c6bc189e63659a701cc8175da5a33c2ad4601d
Ashish2796 <ashishgupta2796@gmail.com>
2021-08-01 17:55:03 +0530

    to configure Spring Security Authorization

--- src/main/java/com/springframework/security/config/CustomWebSecurityConfig.java
+++ src/main/java/com/springframework/security/config/CustomWebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.springframework.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -10,6 +11,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class CustomWebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
+	
+	/* (non-Javadoc)
+	 * For in-memory Authentication
+	 */
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
@@ -25,5 +30,20 @@ public class CustomWebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Bean
 	public PasswordEncoder getPasswordEncoder() {
 		return NoOpPasswordEncoder.getInstance();
+	}
+	
+	
+	/* (non-Javadoc)
+	 * For role based authoriztaion
+	 */
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/admin").hasRole("ADMIN")
+			.antMatchers("/user").hasAnyRole("USER","ADMIN")
+			.antMatchers("/").permitAll()
+			.and()
+			.formLogin();
+			
 	}
 }
